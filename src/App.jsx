1f1e48e7f0e42fa3d816fda3108a64ba6be53ef3
Pawan Pandey <pawnnpandey@gmail.com>
2025-07-11 03:09:33 +0530

    updated context

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { TodoProvider } from './components/TodoContext'
+import { TodoProvider } from './contexts/TodoContext'
 
 function App() {
   const [todos, setTodos] = useState([])
